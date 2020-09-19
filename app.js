45522e2e076ca5d6fe3706184e2f67f2909b8092
Kyle Willard <68755034+Kyleswillard@users.noreply.github.com>
2020-09-19 11:59:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const result = document.getElementById('result')
 
 
 button.onclick = function results() {
-    var price = document.getElementById('vPrice').value
-    var res = Math.floor(price * .0057);
+    let price = document.getElementById('vPrice').value
+    let res = Math.floor(price * .0057);
     result.innerHTML = `Tax Amount: $${res}`;
 }
